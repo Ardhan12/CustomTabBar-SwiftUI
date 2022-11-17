77150f30cc5bd44fb8618248379c117878ca6ce6
Ardhan12 <arief.ramadhan43@gmail.com>
2022-11-17 14:40:07 +0700

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Arief Ramadhan on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
