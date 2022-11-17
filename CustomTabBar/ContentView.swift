f3cda4ffa435c50c3a9a003db685418a310ae25a
Ardhan12 <arief.ramadhan43@gmail.com>
2022-11-17 14:53:46 +0700

    create tab bar and generator

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                Text("home")
+                Spacer()
+                HStack {
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/20, systemIconName: "house", tabName: "Map")
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/20, systemIconName: "plus", tabName: "Add")
+                    
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/20, systemIconName: "bookmark.fill", tabName: "List")
+                }.frame(width: geometry.size.width, height: geometry.size.height/8)
+                    .background(Color("bg").shadow(radius: 2))
+            } .edgesIgnoringSafeArea(.bottom)
         }
-        .padding()
+    }
+}
+
+struct TabBarIcon: View {
+    
+    let width, height: CGFloat
+    let systemIconName, tabName: String
+    
+    
+    var body: some View {
+        VStack {
+            Image(systemName: systemIconName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width, height: height)
+                .padding(.top, 10)
+            Text(tabName)
+                .font(.footnote)
+            Spacer()
+        }
+        .padding(.horizontal, -4)
     }
 }
 
